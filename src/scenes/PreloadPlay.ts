7af98e4286b94c07cfafc224c8129d5599c19f50
Nguyễn Thanh Tú <clangod002@gmail.com>
2021-11-19 10:30:09 +0700

    update

--- src/scenes/PreloadPlay.ts
+++ src/scenes/PreloadPlay.ts
@@ -11,21 +11,39 @@ export default class PreLoadPlay extends Phaser.Scene {
     //load audio
     this.preloadSprite();
     //game Over
-
-    /*Loader Events */
+    let style = { font: "30px Arial", fill: "#000" };
+    let textLoading = this.add.text(320, 300, `Loading...0%}`, style);
     let loadingBar = this.add.graphics({
       fillStyle: {
-        color: 0xffffff, //white
+        color: 0x00000, //black
+      },
+      lineStyle: {
+        color: 0x00000,
+        width: 10,
       },
     });
+
+    /*Loader Events */
     this.load.on("progress", (percent: number) => {
-      loadingBar.fillRect(
-        this.game.renderer.width / 2,
-        0,
-        50,
-        this.game.renderer.height * percent
+      textLoading.setText(`Loading...${Math.floor(percent * 100)}%`);
+      loadingBar.clear();
+
+      loadingBar.strokeRect(
+        175,
+        this.game.renderer.height / 2 - 25,
+        this.game.renderer.width / 2 + 50,
+        100
       );
-      console.log(percent);
+      loadingBar.fillRect(
+        200,
+        this.game.renderer.height / 2,
+        (this.game.renderer.width * percent) / 2,
+        50
+      );
+    });
+    this.load.on("complete", () => {
+      loadingBar.destroy();
+      textLoading.destroy();
     });
   }
   preloadImage() {
@@ -55,10 +73,6 @@ export default class PreLoadPlay extends Phaser.Scene {
     });
   }
   preloadAudio() {
-    this.load.audio(
-      "hehe",
-      "assets/music/238069__shuinvy__childhoodmusicboxhalf.mp3"
-    );
     this.load.audio("jump", "assets/player/sfx/jump.wav");
     this.load.audio("hit", "assets/player/sfx/hit.wav");
   }
